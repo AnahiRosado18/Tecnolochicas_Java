4a301581f9925e3e9edb4a0ded0adc2aa783aead
AnahiRosado18 <131729763+AnahiRosado18@users.noreply.github.com>
2025-04-22 17:01:54 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package Sesion2_Reto01;
+
+public class main {
+
+    public static void main(String[] args) {
+        SimuladorFarmacia simulador = new SimuladorFarmacia();
+        simulador.ingresoInfo();
+        simulador.calcularPrecio();
+        simulador.impresionCalculo();
+    }
+}
